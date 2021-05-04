fee25e9609366a6ac72bdddbe3976ebedb1b2a63
Vu Nguyen Khanh <vunguyenkhanh.6599@gmail.com>
2021-05-04 11:56:39 +0700

    update lab01

---
+++ Lab01/src/lab01/helloWorld.java
@@ -0,0 +1,12 @@
+package lab01;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Xin chao cac ban ! \n");
+        System.out.print("Hello world ! \n");
+        System.out.println("My name is Khanh ! \n");
+	}
+
+}
